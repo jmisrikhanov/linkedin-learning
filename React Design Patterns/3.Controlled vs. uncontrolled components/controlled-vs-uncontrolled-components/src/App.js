4ce6274afb2ff9ac50704db0dec819913484d0bd
jmisrikhanov <jmisrikhanov@gmail.com>
2022-03-28 11:46:31 +0200

    Controlled and Uncontrolled Modals

--- React Design Patterns/3.Controlled vs. uncontrolled components/controlled-vs-uncontrolled-components/src/App.js
+++ React Design Patterns/3.Controlled vs. uncontrolled components/controlled-vs-uncontrolled-components/src/App.js
@@ -1,12 +1,30 @@
 import "./App.css";
 import { ControlledForm } from "./ControlledForm";
+import { UncontrolledModal } from "./UncontrolledModal";
 import { UncontrolledForm } from "./UncontrolledForm";
+import { useState } from "react";
+import { ControlledModal } from "./ControlledModal";
 
 function App() {
+  const [shouldShowModal, setShouldShowModal] = useState(false);
+
   return (
     <div className="App">
       {/* <UncontrolledForm /> */}
-      <ControlledForm />
+      {/* <ControlledForm /> */}
+      {/* <UncontrolledModal /> */}
+
+      <ControlledModal
+        shouldShow={shouldShowModal}
+        onRequestClose={() => {
+          setShouldShowModal(false);
+        }}
+      >
+        <h1>Hello!</h1>
+      </ControlledModal>
+      <button onClick={() => setShouldShowModal(!shouldShowModal)}>
+        {shouldShowModal ? "Hide Modal" : "Show Modal"}
+      </button>
     </div>
   );
 }
