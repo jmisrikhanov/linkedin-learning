6802a072174bd3ed1137ecda18b7cbf662aa5b0f
jmisrikhanov <jmisrikhanov@gmail.com>
2022-03-28 14:33:08 +0200

    printing and loading data

--- React Design Patterns/4.Higher-Order Components/higher-order-components/src/App.js
+++ React Design Patterns/4.Higher-Order Components/higher-order-components/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { printProps } from "./printProps";
+import { UserInfo } from "./UserInfo";
+import { withUser } from "./withUser";
+
+// const UserInfoWrapped = printProps(UserInfo);
+
+const UserInfoWithLoader = withUser(UserInfo, 123);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <UserInfoWrapped a={1} b="Hello" c={{ name: "Shaun" }} />
+
+    <UserInfoWithLoader />
   );
 }
 
