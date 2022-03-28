e1ef2ab80b19978fca784c43e44a8dcc0d59b45e
jmisrikhanov <jmisrikhanov@gmail.com>
2022-03-28 12:16:40 +0200

    Uncontrolled Onboarding Flow

--- React Design Patterns/3.Controlled vs. uncontrolled components/controlled-vs-uncontrolled-components/src/App.js
+++ React Design Patterns/3.Controlled vs. uncontrolled components/controlled-vs-uncontrolled-components/src/App.js
@@ -4,9 +4,29 @@ import { UncontrolledModal } from "./UncontrolledModal";
 import { UncontrolledForm } from "./UncontrolledForm";
 import { useState } from "react";
 import { ControlledModal } from "./ControlledModal";
+import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
 
 function App() {
   const [shouldShowModal, setShouldShowModal] = useState(false);
+
+  const StepOne = ({ goToNext }) => (
+    <>
+      <h1>Step 1</h1>
+      <button onClick={() => goToNext({ name: "John Doe" })}>Next</button>
+    </>
+  );
+  const StepTwo = ({ goToNext }) => (
+    <>
+      <h1>Step 2</h1>
+      <button onClick={() => goToNext({ age: 100 })}>Next</button>
+    </>
+  );
+  const StepThree = ({ goToNext }) => (
+    <>
+      <h1>Step 3</h1>
+      <button onClick={() => goToNext({ hairColor: "Brown" })}>Next</button>
+    </>
+  );
 
   return (
     <div className="App">
@@ -14,7 +34,7 @@ function App() {
       {/* <ControlledForm /> */}
       {/* <UncontrolledModal /> */}
 
-      <ControlledModal
+      {/* <ControlledModal
         shouldShow={shouldShowModal}
         onRequestClose={() => {
           setShouldShowModal(false);
@@ -24,7 +44,18 @@ function App() {
       </ControlledModal>
       <button onClick={() => setShouldShowModal(!shouldShowModal)}>
         {shouldShowModal ? "Hide Modal" : "Show Modal"}
-      </button>
+      </button> */}
+
+      <UncontrolledOnboardingFlow
+        onFinish={(data) => {
+          console.log(data);
+          alert("Onboarding complete!");
+        }}
+      >
+        <StepOne />
+        <StepTwo />
+        <StepThree />
+      </UncontrolledOnboardingFlow>
     </div>
   );
 }
---
+++ React Design Patterns/3.Controlled vs. uncontrolled components/controlled-vs-uncontrolled-components/src/UncontrolledOnboardingFlow.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+export const UncontrolledOnboardingFlow = ({ children, onFinish }) => {
+  const [onboardingData, setOnboardingData] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToNext = (stepData) => {
+    const nextIndex = currentIndex + 1;
+    const updatedData = {
+      ...onboardingData,
+      ...stepData,
+    };
+
+    console.log(updatedData);
+
+    if (nextIndex < children.length) {
+      setCurrentIndex(nextIndex);
+    } else {
+      onFinish(updatedData);
+    }
+
+    setOnboardingData(updatedData);
+  };
+
+  // Basically this just covers us in the case that children is only a single element
+  const currentChild = React.Children.toArray(children)[currentIndex];
+
+  if (React.isValidElement(currentChild)) {
+    return React.cloneElement(currentChild, { goToNext });
+  }
+  return currentChild;
+};
