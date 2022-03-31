eeef9d2674e2cb15d86d7eabe630613baa33c9b5
jmisrikhanov <jmisrikhanov@gmail.com>
2022-03-31 22:59:34 +0200

    withEditableResource

--- React Design Patterns/4.Higher-Order Components/higher-order-components/src/UserInfoForm.js
+++ React Design Patterns/4.Higher-Order Components/higher-order-components/src/UserInfoForm.js
@@ -1,6 +1,45 @@
-import { withEditableUser } from "./withEditabkeUser";
+// import { withEditableUser } from "./withEditabkeUser";
 
-export const UserInfoForm = withEditableUser(
+import { withEditableResource } from "./withEditableResource";
+
+// export const UserInfoForm = withEditableUser(
+//   ({ user, onChangeUser, onSaveUser, onResetUser }) => {
+//     const { name, age, hairColor } = user || {};
+//     return user ? (
+//       <>
+//         <label>
+//           Name:
+//           <input
+//             value={name}
+//             onChange={(e) => onChangeUser({ name: e.target.value })}
+//           />
+//         </label>
+//         <label>
+//           Age:
+//           <input
+//             type="number"
+//             value={age}
+//             onChange={(e) => onChangeUser({ age: Number(e.target.value) })}
+//           />
+//         </label>
+//         <label>
+//           Hair Color:
+//           <input
+//             value={hairColor}
+//             onChange={(e) => onChangeUser({ hairColor: e.target.value })}
+//           />
+//         </label>
+//         <button onClick={onResetUser}>Reset</button>
+//         <button onClick={onSaveUser}>Save Changes</button>
+//       </>
+//     ) : (
+//       <p>Loading...</p>
+//     );
+//   },
+//   "123"
+// );
+
+export const UserInfoForm = withEditableResource(
   ({ user, onChangeUser, onSaveUser, onResetUser }) => {
     const { name, age, hairColor } = user || {};
     return user ? (
@@ -34,5 +73,6 @@ export const UserInfoForm = withEditableUser(
       <p>Loading...</p>
     );
   },
-  "123"
+  "/users/123",
+  "user"
 );
