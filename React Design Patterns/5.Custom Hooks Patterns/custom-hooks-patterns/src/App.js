eb0dfaa6195204e618c603826cf57557b72fd962
jmisrikhanov <jmisrikhanov@gmail.com>
2022-03-31 23:27:26 +0200

    useUser hook

--- React Design Patterns/5.Custom Hooks Patterns/custom-hooks-patterns/src/App.js
+++ React Design Patterns/5.Custom Hooks Patterns/custom-hooks-patterns/src/App.js
@@ -3,7 +3,14 @@ import "./App.css";
 import { UserInfo } from "./UserInfo";
 
 function App() {
-  return <UserInfo />;
+  return (
+    // <UserInfo />
+    <>
+      <UserInfo userId="123" />
+      <UserInfo userId="234" />
+      <UserInfo userId="345" />
+    </>
+  );
 }
 
 export default App;
